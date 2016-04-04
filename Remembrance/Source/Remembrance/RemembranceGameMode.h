808a43b0019763590a25bed22b76d6aa7ca6d65f
glitch880 <a14eligr@hs.local>
2016-04-04 11:34:18 +0200

    commit 2 (Sauce files)
    Yoloing it up

---
+++ Remembrance/Source/Remembrance/RemembranceGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "RemembranceGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARemembranceGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ARemembranceGameMode();
+};
+
+
+
